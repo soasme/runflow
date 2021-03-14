f32e510ceb0728a0104ffcd97feba7590bac98c7
soasme <soasme@gmail.com>
2021-03-14 23:09:52 +1300

    build task from cli.

---
+++ ROADMAP.md
@@ -0,0 +1,5 @@
+# ROAD MAP
+
+- [ ] Implement Task data structure.
+- [ ] Start starlette and apscheduler.
+- [x] Can build a task via CLI.
--- jetflow/cli.py
+++ jetflow/cli.py
@@ -13,7 +13,25 @@ def entry(log_level):
 @entry.command()
 @click.argument('module')
 def build(module):
-    logger.info(f'build task - {module}')
+    logger.info(f'building task - {module}')
+    from .utils import import_string
+    module = import_string(module)
+
+    if not callable(module):
+        click.echo('build task failed - not runnable', err=True)
+        click.abort(1)
+
+    instance = module()
+
+    if hasattr(instance, 'run'):
+        instance.run()
+        return
+
+    if hasattr(instance, '__call__'):
+        instance()
+        return
+
+
 
 @entry.command()
 def worker():
