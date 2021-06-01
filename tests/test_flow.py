04c794a4ee1da7d5f67a88aa4caf71403972d237
soasme <soasme@gmail.com>
2021-06-01 21:19:41 +1200

    add test case.

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -10,3 +10,14 @@ flow "hello-world" {
     """)
     out, err = capsys.readouterr()
     assert out == 'hello world\n'
+
+def test_hello_world2(capsys):
+    runflow("""
+flow "hello-world2" {
+  task "command" "echo" {
+    command = ["echo", "hello", "world"]
+  }
+}
+    """)
+    out, err = capsys.readouterr()
+    assert out == 'hello world\n'
