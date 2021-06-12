4883341eaf2de4e4f9a4ebec4d957b17e9292443
soasme <soasme@gmail.com>
2021-06-12 21:55:34 +1200

    contribs: http_request: add basic test cases.

---
+++ tests/contribs/test_http.py
@@ -0,0 +1,24 @@
+import json
+import pytest
+import docker
+import runflow
+
+def test_http_get(tmpdir, capsys):
+    flow = tmpdir / "test.rf"
+    out = tmpdir / "out.txt"
+    with open('examples/http_get.hcl') as f:
+        flow.write(f.read())
+    runflow.runflow(flow, {'out': out})
+
+    data = json.loads(out.read())
+
+    assert data['name'] == 'runflow'
+
+def test_http_post(tmpdir, capsys):
+    flow = tmpdir / "test.rf"
+    out = tmpdir / "out.txt"
+    with open('examples/http_post.hcl') as f:
+        flow.write(f.read())
+    runflow.runflow(flow, {'out': out})
+
+    assert '<h1>' in out.read()
