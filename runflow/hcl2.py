1845ccaddc26ea83db1211216c6283d13f58b0ce
soasme <soasme@gmail.com>
2021-06-14 22:52:03 +1200

    core: extract Attribute/Block merge_to.

--- runflow/hcl2.py
+++ runflow/hcl2.py
@@ -15,32 +15,46 @@ from hcl2.lark_parser import Lark_StandAlone
 from hcl2.transformer import DictTransformer as _DictTransformer
 
 class Attribute(dict):
-    pass
+
+    def merge_to(self, res):
+        res = dict(res)
+        res.update(self)
+        return res
+
+class Block(dict):
+
+    def merge_to(self, res):
+        res = dict(res)
+        for key, value in self.items():
+            key = str(key)
+
+            if key not in res:
+                res[key] = [value]
+                continue
+
+            if not isinstance(res[key], list):
+                res[key] = [res[key], value]
+                continue
+
+            if isinstance(value, list):
+                res[key].extend(value)
+            else:
+                res[key].append(value)
+        return res
 
 class DictTransformer(_DictTransformer):
 
     def attribute(self, args: List) -> Attribute:
         return Attribute(super().attribute(args))
 
+    def block(self, args: List) -> Block:
+        return Block(super().block(args))
+
     def body(self, args: List) -> Dict[str, List]:
         args = self.strip_new_line_tokens(args)
         result: Dict[str, Any] = {}
         for arg in args:
-            if isinstance(arg, Attribute):
-                result.update(arg)
-                continue
-            for key, value in arg.items():
-                key = str(key)
-                if key not in result:
-                    result[key] = [value]
-                else:
-                    if isinstance(result[key], list):
-                        if isinstance(value, list):
-                            result[key].extend(value)
-                        else:
-                            result[key].append(value)
-                    else:
-                        result[key] = [result[key], value]
+            result = arg.merge_to(result)
         return result
 
 hcl2 = Lark_StandAlone()
