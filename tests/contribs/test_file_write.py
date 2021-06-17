34209769e6ae43bafa7bab750bc39a87d455f105
soasme <soasme@gmail.com>
2021-06-18 00:29:43 +1200

    test: add test case for examples/file_write_stdout.hcl.

--- tests/contribs/test_file_write.py
+++ tests/contribs/test_file_write.py
@@ -13,3 +13,8 @@ def test_file_write(tmpdir, capsys):
     runflow.runflow(flow, vars={'out': out})
 
     assert out.read() == 'foo bar'
+
+def test_file_write_stdout(tmpdir, capsys):
+    runflow.runflow(path='examples/file_write_stdout.hcl')
+    out, err = capsys.readouterr()
+    assert out == '{"web_proxy": {"proxy_host": "127.0.0.1", "proxy_port": 8964}}\n'
