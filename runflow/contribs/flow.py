b1f3039c3d686b0e0d726ff9e442da1bdebd47a9
soasme <soasme@gmail.com>
2021-06-21 15:11:42 +1200

    contribs: flow_run: new task type.

---
+++ runflow/contribs/flow.py
@@ -0,0 +1,10 @@
+from runflow.core import load_flow
+
+class FlowRunTask:
+
+    def __init__(self, path=None, source=None, module=None, vars=None):
+        self.flow = load_flow(path=path, source=source, module=module)
+        self.vars = vars or {}
+
+    async def run(self, context):
+        await self.flow.run(vars=self.vars)
