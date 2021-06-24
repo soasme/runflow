fb174464a85268e9885f882fb11b7153592a9313
soasme <soasme@gmail.com>
2021-06-25 11:58:51 +1200

    contribs: flow_run: export flow context.

--- runflow/contribs/flow.py
+++ runflow/contribs/flow.py
@@ -1,10 +1,18 @@
 from runflow import loadflow
+from runflow.hcl2 import loads, evaluate
 
 
 class FlowRunTask:
-    def __init__(self, path=None, source=None, module=None, vars=None):
+    def __init__(
+        self, path=None, source=None, module=None, vars=None, exports=None
+    ):
         self.flow = loadflow(path=path, source=source, module=module)
         self.vars = vars or {}
+        self.exports = exports or {}
 
     async def run(self, context):
-        await self.flow.run(vars=self.vars)
+        flow_context = await self.flow.run(vars=self.vars)
+        return {
+            key: evaluate(loads(value, "eval"), flow_context)
+            for key, value in self.exports.items()
+        }
