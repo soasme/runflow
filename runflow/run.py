de87903aa7fd499ca49446fad8ca086c6ece32fc
soasme <soasme@gmail.com>
2021-07-04 16:08:19 +1200

    core: remove loadflow func.

--- runflow/run.py
+++ runflow/run.py
@@ -1,25 +1,14 @@
 """Run a flow spec."""
 
 from .core import Flow
-from .utils import import_module, run_async
-
-
-def loadflow(path=None, source=None, module=None, flow=None):
-    """Load a flow object."""
-    if path:
-        flow = Flow.from_specfile(path)
-    elif source:
-        flow = Flow.from_spec(source)
-    elif module:
-        flow = import_module(module)
-    return flow
+from .utils import run_async
 
 
 async def runflow_async(
     path=None, source=None, module=None, flow=None, vars=None
 ):
     """Run a flow object (async)."""
-    _flow = loadflow(path=path, source=source, module=module, flow=flow)
+    _flow = Flow.load(path=path, source=source, module=module, flow=flow)
     assert _flow and isinstance(_flow, Flow)
     await _flow.run(vars or {})
 
