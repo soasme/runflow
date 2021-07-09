36c111f3d37e0fdbd7e9ddf849a528850ebf70b7
soasme <soasme@gmail.com>
2021-07-09 14:14:09 +1200

    http: validate http payloads.

--- tests/contribs/test_http.py
+++ tests/contribs/test_http.py
@@ -7,21 +7,40 @@ import runflow
 
 
 def test_http_get(tmpdir, capsys):
-    flow = tmpdir / "test.hcl"
     out = tmpdir / "out.txt"
-    with open('examples/http_get.hcl') as f:
-        flow.write(f.read())
-    runflow.runflow(flow, vars={'out': out})
-
+    runflow.runflow(path="examples/http_get.hcl", vars={'out': out})
     data = json.loads(out.read())
-
     assert data['name'] == 'runflow'
 
-def test_http_post(tmpdir, capsys):
-    flow = tmpdir / "test.hcl"
-    out = tmpdir / "out.txt"
-    with open('examples/http_post.hcl') as f:
-        flow.write(f.read())
-    runflow.runflow(flow, vars={'out': out})
 
+def test_http_post(tmpdir, capsys):
+    out = tmpdir / "out.txt"
+    runflow.runflow(path="examples/http_post.hcl", vars={'out': out})
     assert '<h1 align="center">' in out.read()
+
+
+def test_invalid_http_payload(capsys):
+    runflow.runflow(source="""
+flow "invalid_http_payload" {
+  task "http_request" "this" {
+    method = "GET"
+    url = 1
+  }
+}
+    """)
+    _, err = capsys.readouterr()
+    assert "'url' must be <class 'str'>" in err
+
+
+def test_invalid_http_payload2(capsys):
+    runflow.runflow(source="""
+flow "invalid_http_payload" {
+  task "http_request" "this" {
+    method = "FETCH"
+    url = "https://github.com"
+  }
+}
+    """)
+    _, err = capsys.readouterr()
+    assert "'method' must be in" in err
+    assert "got 'FETCH'" in err
