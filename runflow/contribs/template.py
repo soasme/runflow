be6c6a23685ff97a5281eece7cff8f3b3b236f06
soasme <soasme@gmail.com>
2021-07-09 15:04:19 +1200

    template: add docstring for Hcl2TemplateTask.

--- runflow/contribs/template.py
+++ runflow/contribs/template.py
@@ -1,4 +1,10 @@
 class Hcl2TemplateTask:
+    """Render HCL2 template.
+
+    Note: this class is just a placeholder class for hcl2 task system
+    to work. The actual implementation is on `runflow.core`.
+    """
+
     def __init__(self, source, context=None):
         self.source = source
 
