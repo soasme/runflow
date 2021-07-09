d6eeb4cf4df0d234c54e5e97aeb13bc8f84015ed
soasme <soasme@gmail.com>
2021-07-09 15:04:43 +1200

    sql: add argument validators for sql tasks.

--- runflow/contribs/sql.py
+++ runflow/contribs/sql.py
@@ -1,37 +1,77 @@
+from typing import List, Optional, Union
+
+import attr
 from sqlalchemy import create_engine, text
 
 
+@attr.s(
+    auto_attribs=True,
+    kw_only=True,
+    frozen=True,
+)
 class SqlExecTask:
-    def __init__(self, dsn, sql, parameters=None, autocommit=True):
-        self.engine = create_engine(str(dsn), echo=False, future=True)
-        self.sqls = sql
-        self.parameters = parameters or []
-        self.autocommit = autocommit
+    """This task execute sql statement on a sql database."""
+
+    dsn: str = attr.ib(validator=attr.validators.instance_of(str))
+    sql: List[str] = attr.ib(validator=attr.validators.instance_of(list))
+    parameters: Optional[Union[dict, list]] = attr.ib(
+        validator=attr.validators.optional(
+            attr.validators.instance_of((dict, list))
+        ),
+        default=None,
+    )
+    autocommit: bool = True
+
+    # pylint: disable=no-self-use
+    @sql.validator
+    def _check_sql_statement(self, field, value):
+        for sql in value:
+            if not isinstance(sql, dict) or "statement" not in sql:
+                raise ValueError("invalid sql: no sql.statement")
 
     def run(self):
-        with self.engine.begin() as conn:
+        engine = create_engine(str(self.dsn), echo=False, future=True)
+        with engine.begin() as conn:
             conn = conn.execution_options(autocommit=self.autocommit)
-            for sql in self.sqls:
+            for sql in self.sql:
                 statement = text(sql["statement"])
                 parameters = sql.get("parameters") or None
                 conn.execute(statement, parameters)
         return {}
 
 
+@attr.s(
+    auto_attribs=True,
+    kw_only=True,
+    frozen=True,
+)
 class SqlRowTask:
-    def __init__(self, dsn, sql, parameters=None):
-        self.engine = create_engine(str(dsn), echo=False, future=True)
+    """This task fetch data from a sql database."""
 
-        if len(sql) != 1:
-            raise ValueError("sql_row supports only one sql block.")
+    dsn: str = attr.ib(validator=attr.validators.instance_of(str))
+    sql: List[str] = attr.ib(validator=attr.validators.instance_of(list))
+    parameters: Optional[Union[dict, list]] = attr.ib(
+        validator=attr.validators.optional(
+            attr.validators.instance_of((dict, list))
+        ),
+        default=None,
+    )
 
-        self.sql = sql[0]
-        self.parameters = parameters or []
+    # pylint: disable=no-self-use
+    @sql.validator
+    def _check_sql_statement(self, field, value):
+        if len(value) != 1:
+            raise ValueError("invalid sql: require one sql block")
+
+        sql = value[0]
+        if not isinstance(sql, dict) or "statement" not in sql:
+            raise ValueError("invalid sql: no sql.statement")
 
     def run(self):
-        with self.engine.begin() as conn:
-            statement = text(self.sql["statement"])
-            parameters = self.sql.get("parameters") or None
+        engine = create_engine(str(self.dsn), echo=False, future=True)
+        with engine.begin() as conn:
+            statement = text(self.sql[0]["statement"])
+            parameters = self.sql[0].get("parameters") or None
             result = conn.execute(statement, parameters)
             rows = [dict(m) for m in result.mappings()]
             return {"rows": rows}
